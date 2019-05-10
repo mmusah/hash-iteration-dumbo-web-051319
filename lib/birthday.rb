c2ebb7e26cb699a5d225245fdb57b525b223a456
Muhammad Musah <mmusah1@gmail.com>
2019-05-10 09:53:39 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,6 +1,6 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
@@ -10,6 +10,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
+## bonus
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
